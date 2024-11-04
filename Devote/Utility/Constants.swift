030df109648455663e2e9392885f43ce8be898f9
Jozek Andrzej Hajduk Sánchez <hajduksanchez.dev@gmail.com>
2024-11-04 12:27:24 -0500

    Check main aspects of Core Data

---
+++ Devote/Utility/Constants.swift
@@ -0,0 +1,20 @@
+//
+//  Constants.swift
+//  Devote
+//
+//  Created by Jozek Hajduk on 4/11/24.
+//
+
+import SwiftUI
+
+// MARK: - Formatter
+let itemFormatter: DateFormatter = {
+    let formatter = DateFormatter()
+    formatter.dateStyle = .short
+    formatter.timeStyle = .medium
+    return formatter
+}()
+
+// MARK: - UI
+
+// MARK: - UX
